938a72e002e1e1ba811f8bbded51d5035a4a48ba
xfeeros <evgeniyg.sub@gmail.com>
2024-01-03 01:50:08 +0300

    Resolved conflicts

--- Program.cs
+++ Program.cs
@@ -1,53 +1,66 @@
 ﻿using Discord;
 using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
 
 public class Program
 {
-    string token = System.IO.File.ReadAllText("config.txt");
-    private readonly DiscordSocketClient _client;
+    private DiscordSocketClient _client;
 
     public static void Main(string[] args)
-    => new Program().MainAsync().GetAwaiter().GetResult();
+        => new Program().MainAsync().GetAwaiter().GetResult();
 
     public Program()
     {
         _client = new DiscordSocketClient();
-
         _client.Log += LogAsync;
-        _client.MessageReceived += HandleCommandAsync;
+        _client.Ready += ReadyAsync;
+        _client.MessageReceived += MessageReceivedAsync;
     }
 
-    private async Task LogAsync(LogMessage log)
+    private Task LogAsync(LogMessage log)
     {
         Console.WriteLine(log);
+        return Task.CompletedTask;
+    }
+
+    private Task ReadyAsync()
+    {
+        Console.WriteLine("Bot is connected!");
+        return Task.CompletedTask;
     }
 
     public async Task MainAsync()
     {
-        string token = System.IO.File.ReadAllText("Token.txt");
+        string token = "YOUR_BOT_TOKEN_HERE";
         await _client.LoginAsync(TokenType.Bot, token);
         await _client.StartAsync();
         await Task.Delay(-1);
     }
 
-    private async Task HandleCommandAsync(SocketMessage messageParam)
+    private async Task MessageReceivedAsync(SocketMessage message)
     {
-        var message = messageParam as SocketUserMessage;
-        if (message == null) return;
-
         if (message.Content == "!join")
         {
-            var user = message.Author as SocketGuildUser;
-            var voiceChannel = user.VoiceChannel;
-            if (voiceChannel != null)
+            if (message.Author is SocketGuildUser user)
             {
-                var audioClient = await voiceChannel.ConnectAsync();
-                await message.Channel.SendMessageAsync("Присоединился к голосовому каналу!");
+                var voiceChannel = user.VoiceChannel;
+                if (voiceChannel != null)
+                {
+                    var audioClient = await voiceChannel.ConnectAsync();
+                    await message.Channel.SendMessageAsync("Присоединился к голосовому каналу!");
+                }
+                else
+                {
+                    await message.Channel.SendMessageAsync("Вы должны находиться в голосовом канале, чтобы я мог присоединиться!");
+                }
             }
-            else
-            {
-                await message.Channel.SendMessageAsync("Вы должны находиться в голосовом канале, чтобы я мог присоединиться!");
-            }
+        }
+        else if (message.Content == "!roll")
+        {
+            var random = new Random();
+            int result = random.Next(1, 101);
+            await message.Channel.SendMessageAsync($"Выпало число: {result}");
         }
     }
 }
