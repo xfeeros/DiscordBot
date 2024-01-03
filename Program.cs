dd49aa1d22e7612a43a889316f0f287194295b91
Konstantin <apnkel@gmail.com>
2024-01-03 10:42:51 +0300

    fix from sub-branch

--- Program.cs
+++ Program.cs
@@ -1,78 +1,59 @@
 ﻿using Discord;
 using Discord.WebSocket;
-using System;
-using System.Threading.Tasks;
-using Serilog;
+using dotenv.net;
 
 public class Program
 {
-    private DiscordSocketClient _client;
+    private readonly DiscordSocketClient _client;
 
     public static void Main(string[] args)
-        => new Program().MainAsync().GetAwaiter().GetResult();
+    => new Program().MainAsync().GetAwaiter().GetResult();
 
     public Program()
     {
-        Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Debug()
-            .WriteTo.Console()
-            .CreateLogger();
-
         _client = new DiscordSocketClient();
+
         _client.Log += LogAsync;
-        _client.Ready += ReadyAsync;
-        _client.MessageReceived += MessageReceivedAsync;
+        _client.MessageReceived += HandleCommandAsync;
+        _client.UserVoiceStateUpdated += HandleVoiceStatusUpdate;
+ 
     }
 
-    private Task LogAsync(LogMessage log)
+    private async Task LogAsync(LogMessage log)
     {
-        Log.Debug(log.Message);
-        return Task.CompletedTask;
-    }
-
-    private Task ReadyAsync()
-    {
-        Log.Information("Bot is connected!");
-        return Task.CompletedTask;
+        Console.WriteLine(log);
+        
     }
 
     public async Task MainAsync()
     {
-        string token = System.IO.File.ReadAllText("Token.txt");
+        DotEnv.Load();
+
+        var config = DotEnv.Read();
+        string token = config["TOKEN"];
+
         await _client.LoginAsync(TokenType.Bot, token);
         await _client.StartAsync();
         await Task.Delay(-1);
     }
 
-    private async Task MessageReceivedAsync(SocketMessage message)
+    private Task HandleVoiceStatusUpdate(SocketUser user, SocketVoiceState state, SocketVoiceState state2)
     {
-        Log.Information($"Received message: {message.Content} from {message.Author.Username}");
+        if (state.ToString() == "Unknown")
+        {
+            Console.WriteLine($"User: {user.Username} joined to {state2}");
+        } else
+        {
+            Console.WriteLine($"User: {user.Username} left from {state}");
+        }
+        
+        return Task.CompletedTask;
+    }
 
-        if (message.Content == "!join")
-        {
-            if (message.Author is SocketGuildUser user)
-            {
-                var voiceChannel = user.VoiceChannel;
-                if (voiceChannel != null)
-                {
-                    var audioClient = await voiceChannel.ConnectAsync();
-                    await message.Channel.SendMessageAsync("Joined the voice channel!");
-                }
-                else
-                {
-                    await message.Channel.SendMessageAsync("You must be in a voice channel for me to join!");
-                }
-            }
-        }
-        else if (message.Content == "!roll")
-        {
-            var random = new Random();
-            int result = random.Next(1, 101);
-            await message.Channel.SendMessageAsync($"Rolled a number: {result}");
-        }
-        else if (message.Content == "!help")
-        {
-            await message.Channel.SendMessageAsync("Available commands: !join - join a voice channel, !roll - generate a random number.");
-        }
+    private Task HandleCommandAsync(SocketMessage messageParam)
+    {
+        var message = messageParam as SocketUserMessage;
+        Console.WriteLine(message.Content);
+        return Task.CompletedTask;
     }
 }
